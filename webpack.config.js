71d66b19dac9afdbc4b2e2bda2fc21d34cfff1c1
Marco Arduini <mar.arduini@gmail.com>
2020-10-05 14:38:41 +0200

    Add source-map

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'webpack-numbers.js',
   },
+  devtool: 'source-map',
+  optimization: {
+    runtimeChunk: true
+  }
 };
